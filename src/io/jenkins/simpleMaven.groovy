f08f170ddc3e5edb5248f26a09be24d1dcaafb99
R. Tyler Croy <tyler@monkeypox.org>
2016-08-11 15:25:57 -0700

    Add a simpleMaven step with some input from @abayer

---
+++ src/io/jenkins/simpleMaven.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/env groovy
+
+/**
+ *
+ */
+void call(String goals       = 'clean install',
+          String options     = '-B -U -e -Dmaven.test.failure.ignore=true',
+          String jdk         = 'jdk7',
+          String testReports = 'target/surefire-reports/**/*.xml',
+          String artifacts   = 'target/**/*.jar',
+) {
+
+    node {
+        stage 'Checkout'
+        checkout scm
+
+        stage 'Build'
+        withEnv([
+            "JAVA_HOME=${tool jdk}",
+            "PATH+MAVEN=${tool 'mvn'}/bin",
+            "PATH+JAVA=${env.JAVA_HOME}/bin",
+        ]) {
+            sh "mvn ${goals} ${options}"
+        }
+
+        stage 'Archive'
+        junit testReports
+        archiveArtifacts artifacts: artifacts,
+                       fingerprint: true
+    }
+}
