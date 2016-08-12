5fc35b1e1e3abada46377b9a4f242a304f280bdd
R. Tyler Croy <tyler@monkeypox.org>
2016-08-11 17:17:48 -0700

    Use an options map

--- vars/simpleMaven.groovy
+++ vars/simpleMaven.groovy
@@ -3,20 +3,27 @@
 /**
  *
  */
-void call(String goals,
-          String options     = '-B -U -e -Dmaven.test.failure.ignore=true',
-          String version     = '3.3.9',
-          String jdk         = 'jdk-7',
-          String testReports = 'target/surefire-reports/**/*.xml',
-          String artifacts   = 'target/**/*.jar'
-) {
+void call(Map opts = [:], String goals) {
+    Map defaults = [
+        options     : '-B -U -e -Dmaven.test.failure.ignore=true',
+        version     : '3.3.9',
+        jdk         : 'jdk-7',
+        testReports : 'target/surefire-reports/**/*.xml',
+        artifacts   : 'target/**/*.jar',
+        defineStages: true,
+    ]
+    Map options = defaults << opts
 
     node('docker') {
-        stage 'Checkout'
+        if (options.defineStages) {
+            stage "Checkout ${options.jdk}"
+        }
         /* make sure we have our source tree on this node */
         checkout scm
 
-        stage 'Build'
+        if (options.defineStages) {
+            stage "Build ${options.jdk}"
+        }
         /* https://github.com/carlossg/docker-maven#running-as-non-root */
         def runArgs = '-v $HOME/.m2:/var/maven/.m2'
         /* Make sure our ~/.m2 directory is there, if we let Docker create it,
@@ -25,15 +32,17 @@ void call(String goals,
         sh 'mkdir -p $HOME/.m2'
 
         /* invoke maven inside of the official container */
-        docker.image("maven:${version}-${jdk}").inside(runArgs) {
+        docker.image("maven:${options.version}-${options.jdk}").inside(runArgs) {
             timestamps {
-                sh "mvn ${goals} ${options} -Duser.home=/var/maven"
+                sh "mvn ${goals} ${options.options} -Duser.home=/var/maven"
             }
         }
 
-        stage 'Archive'
-        junit testReports
-        archiveArtifacts artifacts: artifacts,
+        if (options.defineStages) {
+            stage "Archive ${options.jdk}"
+        }
+        junit options.testReports
+        archiveArtifacts artifacts: options.artifacts,
                        fingerprint: true
     }
 }
