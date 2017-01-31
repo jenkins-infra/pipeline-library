5dcc0876dd97469d95f85375848c912d95e2ffd2
Jesse Glick <jglick@cloudbees.com>
2017-01-31 16:49:36 -0500

    Allow Jenkins versions to be specified as a new axis.

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -6,14 +6,16 @@
 def call(List<Integer> jdkVersions = [8],
         String repo = null,
         Boolean failFast = true,
-        List<String> platforms = ['linux', 'windows']) {
+        List<String> platforms = ['linux', 'windows'],
+        List<String> jenkinsVersions = [null]) {
     Map tasks = [:]
-
     for (int i = 0; i < platforms.size(); ++i) {
         for (int j = 0; j < jdkVersions.size(); ++j) {
+        for (int k = 0; k < jenkinsVersions.size(); ++k) {
             String label = platforms[i]
             String jdk = jdkVersions[j]
-            String stageIdentifier = "${label}-${jdk}"
+            String jenkinsVersion = jenkinsVersions[k]
+            String stageIdentifier = "${label}-${jdk}${jenkinsVersion ? '-' + jenkinsVersion : ''}"
 
             tasks["${label}-${jdk}"] = {
                 node(label) {
@@ -41,6 +43,9 @@ def call(List<Integer> jdkVersions = [8],
                             '-Dmaven.test.failure.ignore=true',
                             "-DskipAfterFailureCount=${failFast}",
                         ]
+                        if (jenkinsVersion) {
+                            mavenOptions += "-Djenkins.version=${jenkinsVersion}"
+                        }
                         String mavenCommand = "mvn ${mavenOptions.join(' ')} clean install"
                         String jdkTool = "jdk${jdk}"
 
@@ -71,6 +76,7 @@ def call(List<Integer> jdkVersions = [8],
                     }
                 }
             }
+        }
         }
     }
 
