f879013fc3bc0ae941537943631bb9bd000db711
Jesse Glick <jglick@cloudbees.com>
2017-02-01 17:59:43 -0500

    @rtyler requests that we just drop compatibility for the old argument form.

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -3,70 +3,80 @@
 /**
  * Simple wrapper step for building a plugin
  */
-def call(List<Integer> jdkVersions = [8],
-        String repo = null,
-        Boolean failFast = true,
-        List<String> platforms = ['linux', 'windows']) {
-    Map tasks = [:]
-
+def call(Map params = [:]) {
+    def platforms = params.containsKey('platforms') ? params.platforms : ['linux', 'windows']
+    def jdkVersions = params.containsKey('jdkVersions') ? params.jdkVersions : [8]
+    def jenkinsVersions = params.containsKey('jenkinsVersions') ? params.jenkinsVersions : [null]
+    def repo = params.containsKey('repo') ? params.repo : null
+    def failFast = params.containsKey('failFast') ? params.failFast : true
+    Map tasks = [failFast: failFast]
     for (int i = 0; i < platforms.size(); ++i) {
         for (int j = 0; j < jdkVersions.size(); ++j) {
-            String label = platforms[i]
-            String jdk = jdkVersions[j]
-            String stageIdentifier = "${label}-${jdk}"
+            for (int k = 0; k < jenkinsVersions.size(); ++k) {
+                String label = platforms[i]
+                String jdk = jdkVersions[j]
+                String jenkinsVersion = jenkinsVersions[k]
+                String stageIdentifier = "${label}-${jdk}${jenkinsVersion ? '-' + jenkinsVersion : ''}"
 
-            tasks["${label}-${jdk}"] = {
-                node(label) {
-                    stage("Checkout (${stageIdentifier})") {
-                        if (env.BRANCH_NAME) {
-                            timestamps {
-                                checkout scm
-                            }
-                        }
-                        else if ((env.BRANCH_NAME == null) && (repo)) {
-                            timestamps {
-                                git repo
-                            }
-                        }
-                        else {
-                            error 'buildPlugin must be used as part of a Multibranch Pipeline *or* a `repo` argument must be provided'
-                        }
-                    }
-
-                    stage("Build (${stageIdentifier})") {
-                        List<String> mavenOptions = [
-                            '--batch-mode',
-                            '--errors',
-                            '--update-snapshots',
-                            '-Dmaven.test.failure.ignore=true',
-                            "-DskipAfterFailureCount=${failFast}",
-                        ]
-                        String mavenCommand = "mvn ${mavenOptions.join(' ')} clean install"
-                        String jdkTool = "jdk${jdk}"
-
-                        withEnv([
-                            "JAVA_HOME=${tool jdkTool}",
-                            "PATH+MAVEN=${tool 'mvn'}/bin",
-                            'PATH+JAVA=${JAVA_HOME}/bin',
-                        ]) {
-                            if (isUnix()) {
+                tasks[stageIdentifier] = {
+                    node(label) {
+                        stage("Checkout (${stageIdentifier})") {
+                            if (env.BRANCH_NAME) {
                                 timestamps {
-                                    sh mavenCommand
+                                    checkout scm
+                                }
+                            }
+                            else if ((env.BRANCH_NAME == null) && (repo)) {
+                                timestamps {
+                                    git repo
                                 }
                             }
                             else {
-                                timestamps {
-                                    bat mavenCommand
+                                error 'buildPlugin must be used as part of a Multibranch Pipeline *or* a `repo` argument must be provided'
+                            }
+                        }
+
+                        stage("Build (${stageIdentifier})") {
+                            List<String> mavenOptions = [
+                                '--batch-mode',
+                                '--errors',
+                                '--update-snapshots',
+                                '-Dmaven.test.failure.ignore=true',
+                                "-DskipAfterFailureCount=${failFast}",
+                            ]
+                            if (jenkinsVersion) {
+                                mavenOptions += "-Djenkins.version=${jenkinsVersion}"
+                            }
+                            String mavenCommand = "mvn ${mavenOptions.join(' ')} clean install"
+                            String jdkTool = "jdk${jdk}"
+
+                            withEnv([
+                                "JAVA_HOME=${tool jdkTool}",
+                                "PATH+MAVEN=${tool 'mvn'}/bin",
+                                'PATH+JAVA=${JAVA_HOME}/bin',
+                            ]) {
+                                if (isUnix()) {
+                                    timestamps {
+                                        sh mavenCommand
+                                    }
+                                }
+                                else {
+                                    timestamps {
+                                        bat mavenCommand
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    stage("Archive (${stageIdentifier})") {
-                        timestamps {
-                            junit '**/target/surefire-reports/**/*.xml'
-                            archiveArtifacts artifacts: '**/target/*.hpi,**/target/*.jpi',
-                                        fingerprint: true
+                        stage("Archive (${stageIdentifier})") {
+                            timestamps {
+                                junit '**/target/surefire-reports/**/*.xml'
+                                if (failFast && currentBuild.result == 'UNSTABLE') {
+                                    error 'There were test failures; halting early'
+                                }
+                                archiveArtifacts artifacts: '**/target/*.hpi,**/target/*.jpi',
+                                            fingerprint: true
+                            }
                         }
                     }
                 }
