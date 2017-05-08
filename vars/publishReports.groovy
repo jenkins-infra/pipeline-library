6bc12ad3762b7646f79d17adeb416d07d321f204
R. Tyler Croy <tyler@monkeypox.org>
2017-05-08 16:20:10 -0700

    Add a simple publisReport Global Variable for publishing reports to Azure blob storage

---
+++ vars/publishReports.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/env groovy
+
+/**
+ * Wrapper for publishing reports to the reports host
+ *
+ * See https://issues.jenkins-ci.org/browse/INFRA-947 for more
+ */
+def call(List<String> files, Map params = [:]) {
+    def timeout = params.get('timeout') ?: '60'
+
+    if (!infra.isTrusted()) {
+        error 'Can only call publishReports from within the trusted.ci environment'
+    }
+
+    withCredentials([
+        string(credentialsId: 'azure-reports-access-key', variable: 'AZURE_STORAGE_ACCESS_KEY'),
+    ]) {
+        docker.image('azuresdk/azure-cli-python:0.1.5').inside {
+            for(int i = 0; i < files.size(); ++i) {
+                String filename = files[i]
+                sh "az storage blob upload --account-name=prodjenkinsreports --container=reports --timeout=${timeout} --file=${filename} --name=${filename}"
+            }
+        }
+    }
+
+}
