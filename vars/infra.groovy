57c7d6e3058cc98c32445282528800f8c40f7565
R. Tyler Croy <tyler@monkeypox.org>
2017-06-29 14:07:46 -0700

    Add support for using Docker Hub credentials inside trusted.ci

--- vars/infra.groovy
+++ vars/infra.groovy
@@ -3,3 +3,14 @@
 Boolean isTrusted() {
     return env.JENKINS_URL == 'https://trusted.ci.jenkins.io:1443/'
 }
+
+Object withDockerCredentials(Closure body) {
+    if (isTrusted()) {
+        withCredentials([[$class: 'ZipFileBinding', credentialsId: 'jenkins-dockerhub', variable: 'DOCKER_CONFIG']]) {
+            return body.call()
+        }
+    }
+    else {
+        echo 'Cannot use Docker credentials outside of the trusted environment'
+    }
+}
