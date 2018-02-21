55cad621d9d787b8ce7acdb8229a401aab136855
Raul Arabaolaza <rarabaolaza@cloudbees.com>
2018-02-21 11:35:29 +0100

    [INFRA-1489] First complete implementation

--- vars/runATH.groovy
+++ vars/runATH.groovy
@@ -5,50 +5,138 @@
  */
 
 def call(Map params = [:]) {
-    
+
     def String athUrl = params.get('athUrl', 'https://github.com/jenkinsci/acceptance-test-harness.git')
     def String athRevision = params.get('athRevision', 'master')
     def String metadataFile = params.get('metadataFile', 'essentials.yml')
-    def String jenkins = params.get('jenkins','latest')
-    def String[] platforms = params.get('platforms', ['linux'])
+    def String jenkins = params.get('jenkins', 'latest')
+
+    def isLocalATH = athUrl.startsWith("file://")
+    def isVersionNumber = (jenkins =~ /^(\d+\.)?(\d+\.)?(\*|\d+)$/).matches()
+
+    def mirror = "http://mirrors.jenkins.io/"
+    def defaultCategory = "org.jenkinsci.test.acceptance.junit.SmokeTest"
+    def jenkinsURl = jenkins
+    def metadata
+    def athContainerImage
+
 
     def athSourcesFolder = "athSources"
 
-    stage("Getting ATH sources (") {
-        if (athUrl.startsWith("file://")) { // Deal with already existing ATH sources
+    stage("Getting ATH sources and Jenkins war") {
+        // Start validation
+        if (!fileExists(metadataFile)) {
+            echo "The metadata file does not exist. Current value is ${metadataFile}"
+            return
+        } else {
+            metadata = readYaml(file: metadataFile).ath
+            if (metadata == null) {
+                error "The provided metadata file seems invalid as it does not contain the ath section"
+            }
+            if (metadata.browsers == null) {
+                error "The provided metadata file seems invalid as it does not include the browser property"
+            }
+        }
+        if (!isLocalATH) {
+            def canConnectToGitRepo = sh(script: "git ls-remote --exit-code -h ${athUrl}", returnStatus: true) == 0
+            if (!canConnectToGitRepo) {
+                error "Is not possible to connect to the given athUrl, please review it. Current value for athUrl is ${athUrl}"
+            }
+        }
+        if (isVersionNumber) {
+            jenkinsURl = mirror + "war/${jenkins}/jenkins.war"
+        } else if (jenkins == "latest") {
+            jenkinsURl = mirror + "war/latest/jenkins.war"
+        } else if (jenkins == "latest-rc") {
+            jenkinsURl = mirror + "/war-rc/latest/jenkins.war"
+        } else if (jenkins == "lts") {
+            jenkinsURl = mirror + "war-stable/latest/jenkins.war"
+        } else if (jenkins == "lts-rc") {
+            jenkinsURl = mirror + "war-stable-rc/latest/jenkins.war"
+        }
+
+        def canFindJenkinsWar = sh(script: "curl --output /dev/null --silent --fail -r 0-0 -L '${jenkinsURl}'", returnStatus: true) == 0
+        if (!canFindJenkinsWar) {
+            error "Is not possible to find the given jenkins war file, please review it. Current value for jenkins is ${jenkins}"
+        }
+        // Validation ended
+
+        // ATH
+        if (isLocalATH) { // Deal with already existing ATH sources
             athSourcesFolder = athUrl - "file://"
-        } else { // TODO validate this is a valid git url
+        } else {
             dir(athSourcesFolder) {
                 checkout changelog: true, poll: false, scm: [$class           : 'GitSCM', branches: [[name: "${athRevision}"]],
                                                              userRemoteConfigs: [[url: "${athUrl}"]]]
             }
         }
-
         dir(athSourcesFolder) {
+            def uid = sh(script: "id -u", returnStdout: true)
+            def gid = sh(script: "id -g", returnStdout: true)
+            athContainerImage = docker.build('jenkins/ath', "--build-arg=uid='$uid' --build-arg=gid='$gid' -f src/main/resources/ath-container/Dockerfile .")
+            // We may need to run things in parallel later, so avoid several checkouts
             stash name: "athSources"
         }
-    }
 
-    stage("Getting jenkins war") {
+        // Jenkins war
+        sh("curl -o jenkins.war -L ${jenkinsURl}")
+        stash includes: '*.war', name: 'jenkinsWar'
 
     }
 
     stage("Running ATH") {
-        dir("work") {
-            unstash name: "athSources"
-            def uid = sh(script: "id -u", returnStdout: true)
-            def gid = sh(script: "id -g", returnStdout: true)
-            def image = docker.build('jenkins/ath', "--build-arg=uid='$uid' --build-arg=gid='$gid' -f src/main/resources/ath-container/Dockerfile .")
-            image.inside('-v /var/run/docker.sock:/var/run/docker.sock -e LOCAL_SNAPSHOTS=true -e SHARED_DOCKER_SERVICE=true -u ath-user') {
-                realtimeJUnit(testResults: 'target/surefire-reports/TEST-*.xml', testDataPublishers: [[$class: 'AttachmentPublisher']]) {
-                    sh '''
-                                eval $(./vnc.sh)
-                                ./run.sh firefox latest -Dmaven.test.failure.ignore=true -DforkCount=1 -Dgroups=org.jenkinsci.test.acceptance.junit.SmokeTest -B
-                            '''
-                }
+        def testsToRun = metadata.tests?.join(",")
+        def categoriesToRun = metadata.categories?.join(",")
+        def browsers = metadata.browsers
+        def failFast = metadata.failFast ?: false
+        def rerunCount = metadata.rerunFailingTestsCount ?: 0
+        def localSnapshots = metadata.useLocalSnapshots ?: true
 
+        if (testsToRun == null && categoriesToRun == null) {
+            categoriesToRun = defaultCategory
+        }
+
+        def testingbranches = ["failFast": failFast]
+        for (browser in browsers) {
+            // TODO remove this if else block once other browser are supported by infra
+            if (browser == "firefox") {
+
+                def currentBrowser = browser
+                def containerArgs = "-v /var/run/docker.sock:/var/run/docker.sock -e LOCAL_SNAPSHOTS=${localSnapshots} -e SHARED_DOCKER_SERVICE=true -u ath-user"
+                def commandBase = "./run.sh ${currentBrowser} jenkins.war -Dmaven.test.failure.ignore=true -DforkCount=1 -B -Dsurefire.rerunFailingTestsCount=${rerunCount}"
+
+                if (testsToRun) {
+                    testingbranches["ATH individual tests-${currentBrowser}"] = {
+                        unstash name: "athSources"
+                        unstash name: "jenkinsWar"
+                        def command = commandBase + " -Dtest=${testsToRun}"
+                        athContainerImage.inside(containerArgs) {
+                            realtimeJUnit(testResults: 'target/surefire-reports/TEST-*.xml', testDataPublishers: [[$class: 'AttachmentPublisher']]) {
+                                sh 'eval "$(./vnc.sh)" && ' + command
+                            }
+
+                        }
+                    }
+                }
+                if (categoriesToRun) {
+                    testingbranches["ATH categories-${currentBrowser}"] = {
+                        unstash name: "athSources"
+                        unstash name: "jenkinsWar"
+                        def command = commandBase + " -Dgroups=${categoriesToRun}"
+                        athContainerImage.inside(containerArgs) {
+                            realtimeJUnit(testResults: 'target/surefire-reports/TEST-*.xml', testDataPublishers: [[$class: 'AttachmentPublisher']]) {
+                                sh 'eval "$(./vnc.sh)" && ' + command
+                            }
+
+                        }
+                    }
+                }
+            } else {
+                echo "${browser} is not yet supported"
             }
         }
+
+        parallel testingbranches
     }
 
 }
