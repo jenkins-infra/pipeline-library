7952f63a576e11449d1462d181329bf19c533977
Oleg Nenashev <o.v.nenashev@gmail.com>
2019-03-18 13:58:53 +0100

    buildPlugin() - Switch to the LTS release

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -225,8 +225,7 @@ static List<Map<String, String>> getConfigurations(Map params) {
  * Includes testing Java 8 and 11 on the newest LTS.
  */
 static List<Map<String, String>> recommendedConfigurations() {
-    //TODO: replace by 2.164.1 once it is released
-    def recentLTS = "2.164"
+    def recentLTS = "2.164.1"
     def configurations = [
         [ platform: "linux", jdk: "8", jenkins: null ],
         [ platform: "windows", jdk: "8", jenkins: null ],
