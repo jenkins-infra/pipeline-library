5b7046ebe4c841c784d4c54805ef26352044f954
Tim Jacomb <timjacomb1+github@gmail.com>
2019-08-13 07:33:23 +0100

    Cleanup plugin workspace after build

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -41,132 +41,140 @@ def call(Map params = [:]) {
 
         tasks[stageIdentifier] = {
             node((useAci && label == 'linux') ? (jdk == '8' ? 'maven' : 'maven-11') : label) {
-                timeout(timeoutValue) {
-                    boolean isMaven
-                    // Archive artifacts once with pom declared baseline
-                    boolean doArchiveArtifacts = !jenkinsVersion && !archivedArtifacts
-                    if (doArchiveArtifacts) {
-                        archivedArtifacts = true
-                    }
-                    boolean incrementals // cf. JEP-305
-
-                    stage("Checkout (${stageIdentifier})") {
-                        infra.checkout(repo)
-                        isMaven = fileExists('pom.xml')
-                        incrementals = fileExists('.mvn/extensions.xml') &&
-                                readFile('.mvn/extensions.xml').contains('git-changelist-maven-extension')
-                    }
-
-                    String changelistF
-                    String m2repo
-
-                    stage("Build (${stageIdentifier})") {
-                        String command
-                        if (isMaven) {
-                            m2repo = "${pwd tmp: true}/m2repo"
-                            List<String> mavenOptions = [
-                                    '--update-snapshots',
-                                    "-Dmaven.repo.local=$m2repo",
-                                    '-Dmaven.test.failure.ignore',
-                            ]
-                            if (incrementals) { // set changelist and activate produce-incrementals profile
-                                mavenOptions += '-Dset.changelist'
-                                if (doArchiveArtifacts) { // ask Maven for the value of -rc999.abc123def456
-                                    changelistF = "${pwd tmp: true}/changelist"
-                                    mavenOptions += "help:evaluate -Dexpression=changelist -Doutput=$changelistF"
-                                }
-                            }
-                            if (jenkinsVersion) {
-                                mavenOptions += "-Djenkins.version=${jenkinsVersion} -Daccess-modifier-checker.failOnError=false"
-                            }
-                            if (javaLevel) {
-                                mavenOptions += "-Djava.level=${javaLevel}"
-                            }
-                            if (params?.findbugs?.run || params?.findbugs?.archive) {
-                                mavenOptions += "-Dfindbugs.failOnError=false"
-                            }
-                            if (skipTests) {
-                                mavenOptions += "-DskipTests"
-                            }
-                            if (params?.checkstyle?.run || params?.checkstyle?.archive) {
-                                mavenOptions += "-Dcheckstyle.failOnViolation=false -Dcheckstyle.failsOnError=false"
-                            }
-                            mavenOptions += "clean install"
-                            if (runFindbugs) {
-                                mavenOptions += "findbugs:findbugs"
-                            }
-                            if (runCheckstyle) {
-                                mavenOptions += "checkstyle:checkstyle"
-                            }
-                            infra.runMaven(mavenOptions, jdk, null, null, addToolEnv)
-                        } else {
-                            List<String> gradleOptions = [
-                                    '--no-daemon',
-                                    'cleanTest',
-                                    'build',
-                            ]
-                            command = "gradlew ${gradleOptions.join(' ')}"
-                            if (isUnix()) {
-                                command = "./" + command
-                            }
-                            infra.runWithJava(command, jdk, null, addToolEnv)
-                        }
-                    }
-
-                    stage("Archive (${stageIdentifier})") {
-                        if (!skipTests) {
-                            String testReports
-                            if (isMaven) {
-                                testReports = '**/target/surefire-reports/**/*.xml'
-                            } else {
-                                testReports = '**/build/test-results/**/*.xml'
-                            }
-                            junit testReports
-                            // TODO do this in a finally-block so we capture all test results even if one branch aborts early
-                        }
-                        if (isMaven && archiveFindbugs) {
-                            def fp = [pattern: params?.findbugs?.pattern ?: '**/target/findbugsXml.xml']
-                            if (params?.findbugs?.unstableNewAll) {
-                                fp['unstableNewAll'] = "${params.findbugs.unstableNewAll}"
-                            }
-                            if (params?.findbugs?.unstableTotalAll) {
-                                fp['unstableTotalAll'] = "${params.findbugs.unstableTotalAll}"
-                            }
-                            findbugs(fp)
-                        }
-                        if (isMaven && archiveCheckstyle) {
-                            def cp = [pattern: params?.checkstyle?.pattern ?: '**/target/checkstyle-result.xml']
-                            if (params?.checkstyle?.unstableNewAll) {
-                                cp['unstableNewAll'] = "${params.checkstyle.unstableNewAll}"
-                            }
-                            if (params?.checkstyle?.unstableTotalAll) {
-                                cp['unstableTotalAll'] = "${params.checkstyle.unstableTotalAll}"
-                            }
-                            checkstyle(cp)
-                        }
-                        if (failFast && currentBuild.result == 'UNSTABLE') {
-                            error 'There were test failures; halting early'
-                        }
+                try {
+                    timeout(timeoutValue) {
+                        boolean isMaven
+                        // Archive artifacts once with pom declared baseline
+                        boolean doArchiveArtifacts = !jenkinsVersion && !archivedArtifacts
                         if (doArchiveArtifacts) {
-                            if (incrementals) {
-                                String changelist = readFile(changelistF)
-                                dir(m2repo) {
-                                    fingerprint '**/*-rc*.*/*-rc*.*' // includes any incrementals consumed
-                                    archiveArtifacts artifacts: "**/*$changelist/*$changelist*",
-                                            excludes: '**/*.lastUpdated',
-                                            allowEmptyArchive: true // in case we forgot to reincrementalify
+                            archivedArtifacts = true
+                        }
+                        boolean incrementals // cf. JEP-305
+
+                        stage("Checkout (${stageIdentifier})") {
+                            infra.checkout(repo)
+                            isMaven = fileExists('pom.xml')
+                            incrementals = fileExists('.mvn/extensions.xml') &&
+                                    readFile('.mvn/extensions.xml').contains('git-changelist-maven-extension')
+                        }
+
+                        String changelistF
+                        String m2repo
+
+                        stage("Build (${stageIdentifier})") {
+                            String command
+                            if (isMaven) {
+                                m2repo = "${pwd tmp: true}/m2repo"
+                                List<String> mavenOptions = [
+                                        '--update-snapshots',
+                                        "-Dmaven.repo.local=$m2repo",
+                                        '-Dmaven.test.failure.ignore',
+                                ]
+                                if (incrementals) { // set changelist and activate produce-incrementals profile
+                                    mavenOptions += '-Dset.changelist'
+                                    if (doArchiveArtifacts) { // ask Maven for the value of -rc999.abc123def456
+                                        changelistF = "${pwd tmp: true}/changelist"
+                                        mavenOptions += "help:evaluate -Dexpression=changelist -Doutput=$changelistF"
+                                    }
                                 }
-                                publishingIncrementals = true
+                                if (jenkinsVersion) {
+                                    mavenOptions += "-Djenkins.version=${jenkinsVersion} -Daccess-modifier-checker.failOnError=false"
+                                }
+                                if (javaLevel) {
+                                    mavenOptions += "-Djava.level=${javaLevel}"
+                                }
+                                if (params?.findbugs?.run || params?.findbugs?.archive) {
+                                    mavenOptions += "-Dfindbugs.failOnError=false"
+                                }
+                                if (skipTests) {
+                                    mavenOptions += "-DskipTests"
+                                }
+                                if (params?.checkstyle?.run || params?.checkstyle?.archive) {
+                                    mavenOptions += "-Dcheckstyle.failOnViolation=false -Dcheckstyle.failsOnError=false"
+                                }
+                                mavenOptions += "clean install"
+                                if (runFindbugs) {
+                                    mavenOptions += "findbugs:findbugs"
+                                }
+                                if (runCheckstyle) {
+                                    mavenOptions += "checkstyle:checkstyle"
+                                }
+                                infra.runMaven(mavenOptions, jdk, null, null, addToolEnv)
                             } else {
-                                String artifacts
-                                if (isMaven) {
-                                    artifacts = '**/target/*.hpi,**/target/*.jpi'
-                                } else {
-                                    artifacts = '**/build/libs/*.hpi,**/build/libs/*.jpi'
+                                List<String> gradleOptions = [
+                                        '--no-daemon',
+                                        'cleanTest',
+                                        'build',
+                                ]
+                                command = "gradlew ${gradleOptions.join(' ')}"
+                                if (isUnix()) {
+                                    command = "./" + command
                                 }
-                                archiveArtifacts artifacts: artifacts, fingerprint: true
+                                infra.runWithJava(command, jdk, null, addToolEnv)
                             }
                         }
+
+                        stage("Archive (${stageIdentifier})") {
+                            if (!skipTests) {
+                                String testReports
+                                if (isMaven) {
+                                    testReports = '**/target/surefire-reports/**/*.xml'
+                                } else {
+                                    testReports = '**/build/test-results/**/*.xml'
+                                }
+                                junit testReports
+                                // TODO do this in a finally-block so we capture all test results even if one branch aborts early
+                            }
+                            if (isMaven && archiveFindbugs) {
+                                def fp = [pattern: params?.findbugs?.pattern ?: '**/target/findbugsXml.xml']
+                                if (params?.findbugs?.unstableNewAll) {
+                                    fp['unstableNewAll'] = "${params.findbugs.unstableNewAll}"
+                                }
+                                if (params?.findbugs?.unstableTotalAll) {
+                                    fp['unstableTotalAll'] = "${params.findbugs.unstableTotalAll}"
+                                }
+                                findbugs(fp)
+                            }
+                            if (isMaven && archiveCheckstyle) {
+                                def cp = [pattern: params?.checkstyle?.pattern ?: '**/target/checkstyle-result.xml']
+                                if (params?.checkstyle?.unstableNewAll) {
+                                    cp['unstableNewAll'] = "${params.checkstyle.unstableNewAll}"
+                                }
+                                if (params?.checkstyle?.unstableTotalAll) {
+                                    cp['unstableTotalAll'] = "${params.checkstyle.unstableTotalAll}"
+                                }
+                                checkstyle(cp)
+                            }
+                            if (failFast && currentBuild.result == 'UNSTABLE') {
+                                error 'There were test failures; halting early'
+                            }
+                            if (doArchiveArtifacts) {
+                                if (incrementals) {
+                                    String changelist = readFile(changelistF)
+                                    dir(m2repo) {
+                                        fingerprint '**/*-rc*.*/*-rc*.*' // includes any incrementals consumed
+                                        archiveArtifacts artifacts: "**/*$changelist/*$changelist*",
+                                                excludes: '**/*.lastUpdated',
+                                                allowEmptyArchive: true // in case we forgot to reincrementalify
+                                    }
+                                    publishingIncrementals = true
+                                } else {
+                                    String artifacts
+                                    if (isMaven) {
+                                        artifacts = '**/target/*.hpi,**/target/*.jpi'
+                                    } else {
+                                        artifacts = '**/build/libs/*.hpi,**/build/libs/*.jpi'
+                                    }
+                                    archiveArtifacts artifacts: artifacts, fingerprint: true
+                                }
+                            }
+                        }
+                    }            
+                } finally {
+                    deleteDir()
+
+                    if (env?.NODE_LABELS?.contains("docker")) {
+                        sh "docker system prune --force --all"
                     }
                 }
             }
