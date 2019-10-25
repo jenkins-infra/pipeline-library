9537accc7de9862b733ea34bdfc4556b57e6156e
Alex Earl <slide.o.mix@gmail.com>
2019-10-25 11:19:17 -0700

    Disable ACI usage
    Disabling ACI usage until issues in the ACI Agent Plugin can be resolved.

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -15,8 +15,11 @@ def call(Map params = [:]) {
     def repo = params.containsKey('repo') ? params.repo : null
     def failFast = params.containsKey('failFast') ? params.failFast : true
     def timeoutValue = params.containsKey('timeout') ? params.timeout : 60
-    def useAci = params.containsKey('useAci') ? params.useAci : false
-    def forceAci = params.containsKey('forceAci') ? params.forceAci : false
+    // disabling ACI usage until the plugin is more robust
+    //def useAci = params.containsKey('useAci') ? params.useAci : false
+    def useAci = false
+    def forceAci = false
+    
     if(timeoutValue > 180) {
       echo "Timeout value requested was $timeoutValue, lowering to 180 to avoid Jenkins project's resource abusive consumption"
       timeoutValue = 180
