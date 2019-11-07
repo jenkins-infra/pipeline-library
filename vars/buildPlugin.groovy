ad21b0a536a03ced22ca5ba5effbda0037e61331
Mark Waite <mark.earl.waite@gmail.com>
2019-11-07 06:49:09 -0700

    Reduce recommended configurations to best bug finders
    Assure that the recommended configuration will test most likely bug
    finding pairs of:
    
    * JDK 8, JDK 11
    * Windows, Linux
    * minimum Jenkins version, recent LTS Jenkins version.
    
    Don't need to cover every combination, just assure that most interesting
    pairs are tested.
    
    Will reduce the load on ci.jenkins.io and may decrease pull request
    build time.

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -252,12 +252,16 @@ List<Map<String, String>> getConfigurations(Map params) {
 static List<Map<String, String>> recommendedConfigurations() {
     def recentLTS = "2.164.1"
     def configurations = [
+        // Intentionally test configurations which have detected the most problems
+        // Linux - Java 8 with plugin specified minimum Jenkins version
+        // Windows - Java 8 with recent LTS
+        // Linux - Java 11 with recent LTS
         [ platform: "linux", jdk: "8", jenkins: null ],
-        [ platform: "windows", jdk: "8", jenkins: null ],
-        [ platform: "linux", jdk: "8", jenkins: recentLTS, javaLevel: "8" ],
+        // [ platform: "windows", jdk: "8", jenkins: null ],
+        // [ platform: "linux", jdk: "8", jenkins: recentLTS, javaLevel: "8" ],
         [ platform: "windows", jdk: "8", jenkins: recentLTS, javaLevel: "8" ],
         [ platform: "linux", jdk: "11", jenkins: recentLTS, javaLevel: "8" ],
-        [ platform: "windows", jdk: "11", jenkins: recentLTS, javaLevel: "8" ]
+        // [ platform: "windows", jdk: "11", jenkins: recentLTS, javaLevel: "8" ]
     ]
     return configurations
 }
