a881c296e85acc4b4d778733ccecef308885f38f
Alex Earl <slide.o.mix@gmail.com>
2020-07-20 13:58:23 -0700

    Remove forceAci
    Removed forceAci as a parameter to the script.

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -15,8 +15,7 @@ def call(Map params = [:]) {
     def repo = params.containsKey('repo') ? params.repo : null
     def failFast = params.containsKey('failFast') ? params.failFast : true
     def timeoutValue = params.containsKey('timeout') ? params.timeout : 60
-    def forceAci = params.containsKey('forceAci') ? params.forceAci : false
-    def useAci = params.containsKey('useAci') ? params.useAci : forceAci
+    def useAci = params.containsKey('useAci') ? params.useAci : false
     if(timeoutValue > 180) {
       echo "Timeout value requested was $timeoutValue, lowering to 180 to avoid Jenkins project's resource abusive consumption"
       timeoutValue = 180
