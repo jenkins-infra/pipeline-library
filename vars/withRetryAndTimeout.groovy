dbbe4249bfd5bb80b1ee9146ac9a042e2ecab12b
Damien Duportal <damien.duportal@gmail.com>
2021-06-01 17:45:23 +0200

    feat: add a new global library 'withRetryAndTimeout' to allow circuit breaking pipeline steps
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ vars/withRetryAndTimeout.groovy
@@ -0,0 +1,31 @@
+
+def call(Map userConfig = [:], Closure body) {
+  def defaultConfig = [
+    retries: 3,
+    bodyTimeout: 10,
+    bodyTimeoutUnit: 'MINUTES',
+  ]
+
+  // Merging the 2 maps - https://blog.mrhaki.com/2010/04/groovy-goodness-adding-maps-to-map_21.html
+  final Map finalConfig = defaultConfig << userConfig
+
+  def counter = 0
+
+  // From https://issues.jenkins.io/browse/JENKINS-51454?focusedCommentId=389893&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-389893
+  retry(finalConfig.retries) {
+    counter += 1
+    try {
+      timeout(time: finalConfig.bodyTimeout, unit: finalConfig.bodyTimeoutUnit) {
+        withEnv(["PIPELINE_RETRY_COUNTER=${counter}"]) {
+          body()
+        }
+      }
+    } catch(org.jenkinsci.plugins.workflow.steps.FlowInterruptedException ex){
+      def causes = ex.getCauses()
+      if(causes.find { ! (it instanceof org.jenkinsci.plugins.workflow.steps.TimeoutStepExecution$ExceededTimeout) }) {
+        throw ex
+      }
+      throw new RuntimeException(ex)
+    }
+  }
+}
