4e836e5ee2d3a540d4e044f916404461a0218a5b
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-03-24 11:46:57 +0100

    Update vars/infra.groovy
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- vars/infra.groovy
+++ vars/infra.groovy
@@ -35,7 +35,7 @@ Object checkout(String repo = null) {
 
 Object checkoutSCM(String repo = null) {
     // Enable long paths to avoid problems with tests on Windows agents 
-    git config core.longpaths true
+    sh 'git config core.longpaths true'
 
     if (env.BRANCH_NAME) {
         checkout scm
