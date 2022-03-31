8d3350e64e946381d86e08673133e0d4943bb7e5
Hervé Le Meur <hlemeur@cloudbees.com>
2022-03-31 16:34:27 +0200

    moar debug

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -138,8 +138,10 @@ def call(userConfig = [:]) {
                   // Convert the report as github comment
                   if (commentReport) {
                     sh 'infracost output --path infracost.json --format github-comment --show-skipped --out-file github.md'
-                    sh 'export INFRACOST_REPORT=$(cat github.md)'
+                    // sh 'export INFRACOST_REPORT=$(cat github.md)'
+                    sh 'export INFRACOST_REPORT="stuff"'
                     pullRequest.comment(env.INFRACOST_REPORT)
+                    sh 'env|sort'
                   }
                   // // Compare the outputs of the two methods
                   // if (commentComparison) {
