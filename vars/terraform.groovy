eebc3d1a8fa393db40acff13f5417730313e53f9
Hervé Le Meur <hlemeur@cloudbees.com>
2022-03-31 18:08:24 +0200

    remove env|sort

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -117,7 +117,6 @@ def call(userConfig = [:]) {
                 withCredentials([string(credentialsId: 'infracost-api-key', variable: 'INFRACOST_API_KEY')]) {
                   Boolean commentReport = false
                   Boolean commentComparison = false
-                  sh 'env|sort' // debut CHANGE_URL empty
                   final String gitUrl = env.CHANGE_URL
                   // On AWS we can use the terraform plan to estimate the costs as it doesn't contains most sensible secrets
                   if (gitUrl.contains('jenkins-infra/aws')) {
