fae2d58ed60930c0b1c4e2282cea8ca3ed70ef2c
Basil Crow <me@basilcrow.com>
2022-04-19 23:22:02 -0700

    Format repository (#356)

--- test/groovy/ParallelDockerUpdatecliStepTests.groovy
+++ test/groovy/ParallelDockerUpdatecliStepTests.groovy
@@ -161,16 +161,16 @@ class ParallelDockerUpdatecliStepTests extends BaseTest {
     // Note: imageName & rebuildImageOnPeriodicJob have already been tested in other tests
     addEnvVar('BRANCH_IS_PRIMARY', 'true')
     script.call(
-      imageName: testImageName,
-      updatecliApplyCronTriggerExpression: anotherCronTriggerExpression,
-      updatecliConfig: [
-        containerMemory: anotherContainerMemory,
-      ],
-      buildDockerConfig: [
-        useContainer: false,
-      ],
-      updatecliCredentialsId: anotherCredentialsId
-    )
+        imageName: testImageName,
+        updatecliApplyCronTriggerExpression: anotherCronTriggerExpression,
+        updatecliConfig: [
+          containerMemory: anotherContainerMemory,
+        ],
+        buildDockerConfig: [
+          useContainer: false,
+        ],
+        updatecliCredentialsId: anotherCredentialsId
+        )
     printCallStack()
 
     // Then we expect a successfull build
@@ -212,10 +212,10 @@ class ParallelDockerUpdatecliStepTests extends BaseTest {
     // When the "parallelDockerUpdatecli" function is called with custom parameters, including legcay parameters for updatecli
     addEnvVar('BRANCH_IS_PRIMARY', 'true')
     script.call(
-      imageName: testImageName,
-      containerMemory: anotherContainerMemory,
-      updatecliApplyCronTriggerExpression: anotherCronTriggerExpression,
-    )
+        imageName: testImageName,
+        containerMemory: anotherContainerMemory,
+        updatecliApplyCronTriggerExpression: anotherCronTriggerExpression,
+        )
     printCallStack()
 
     // Then a successfull build is expected
--- test/groovy/io/jenkins/infra/InfraConfigTest.groovy
+++ test/groovy/io/jenkins/infra/InfraConfigTest.groovy
@@ -10,63 +10,62 @@ import static org.junit.Assert.assertEquals
 
 class InfraConfigTest {
 
-    @Test
-    void canHandleCiConfiguration() throws Exception {
-      def infraConfig = new InfraConfig([JENKINS_URL: 'https://ci.jenkins.io/'])
+  @Test
+  void canHandleCiConfiguration() throws Exception {
+    def infraConfig = new InfraConfig([JENKINS_URL: 'https://ci.jenkins.io/'])
 
-      assertFalse(infraConfig.isTrusted())
-      assertFalse(infraConfig.isInfra())
-      assertTrue(infraConfig.isRunningOnJenkinsInfra())
-      assertEquals('jenkins4eval', infraConfig.getDockerRegistry())
-    }
+    assertFalse(infraConfig.isTrusted())
+    assertFalse(infraConfig.isInfra())
+    assertTrue(infraConfig.isRunningOnJenkinsInfra())
+    assertEquals('jenkins4eval', infraConfig.getDockerRegistry())
+  }
 
-    @Test
-    void canHandleTrustedConfiguration() throws Exception {
-      def infraConfig = new InfraConfig([JENKINS_URL: 'https://trusted.ci.jenkins.io/'])
+  @Test
+  void canHandleTrustedConfiguration() throws Exception {
+    def infraConfig = new InfraConfig([JENKINS_URL: 'https://trusted.ci.jenkins.io/'])
 
-      assertTrue(infraConfig.isTrusted())
-      assertFalse(infraConfig.isInfra())
-      assertTrue(infraConfig.isRunningOnJenkinsInfra())
-      assertEquals('jenkinsciinfra', infraConfig.getDockerRegistry())
-    }
+    assertTrue(infraConfig.isTrusted())
+    assertFalse(infraConfig.isInfra())
+    assertTrue(infraConfig.isRunningOnJenkinsInfra())
+    assertEquals('jenkinsciinfra', infraConfig.getDockerRegistry())
+  }
 
-    @Test
-    void canHandleInfraConfiguration() throws Exception {
-      def infraConfig = new InfraConfig([JENKINS_URL: 'https://infra.ci.jenkins.io/'])
+  @Test
+  void canHandleInfraConfiguration() throws Exception {
+    def infraConfig = new InfraConfig([JENKINS_URL: 'https://infra.ci.jenkins.io/'])
 
-      assertFalse(infraConfig.isTrusted())
-      assertTrue(infraConfig.isInfra())
-      assertTrue(infraConfig.isRunningOnJenkinsInfra())
-      assertEquals('jenkinsciinfra', infraConfig.getDockerRegistry())
-    }
+    assertFalse(infraConfig.isTrusted())
+    assertTrue(infraConfig.isInfra())
+    assertTrue(infraConfig.isRunningOnJenkinsInfra())
+    assertEquals('jenkinsciinfra', infraConfig.getDockerRegistry())
+  }
 
-    @Test
-    void canHandleAnotherInstanceConfiguration() throws Exception {
-      def infraConfig = new InfraConfig([JENKINS_URL: 'https://ci.quidditch.io'])
+  @Test
+  void canHandleAnotherInstanceConfiguration() throws Exception {
+    def infraConfig = new InfraConfig([JENKINS_URL: 'https://ci.quidditch.io'])
 
-      assertFalse(infraConfig.isTrusted())
-      assertFalse(infraConfig.isInfra())
-      assertFalse(infraConfig.isRunningOnJenkinsInfra())
-      assertEquals('jenkins4eval', infraConfig.getDockerRegistry())
-    }
+    assertFalse(infraConfig.isTrusted())
+    assertFalse(infraConfig.isInfra())
+    assertFalse(infraConfig.isRunningOnJenkinsInfra())
+    assertEquals('jenkins4eval', infraConfig.getDockerRegistry())
+  }
 
-    @Test
-    void canHandleDefaultConstructor() throws Exception {
-      def infraConfig = new InfraConfig()
+  @Test
+  void canHandleDefaultConstructor() throws Exception {
+    def infraConfig = new InfraConfig()
 
-      assertFalse(infraConfig.isTrusted())
-      assertFalse(infraConfig.isInfra())
-      assertFalse(infraConfig.isRunningOnJenkinsInfra())
-      assertEquals('jenkins4eval', infraConfig.getDockerRegistry())
-    }
+    assertFalse(infraConfig.isTrusted())
+    assertFalse(infraConfig.isInfra())
+    assertFalse(infraConfig.isRunningOnJenkinsInfra())
+    assertEquals('jenkins4eval', infraConfig.getDockerRegistry())
+  }
 
-    void canHandleUnsetJenkinsUrl() throws Exception {
-      def infraConfig = new InfraConfig([:])
+  void canHandleUnsetJenkinsUrl() throws Exception {
+    def infraConfig = new InfraConfig([:])
 
-      assertFalse(infraConfig.isTrusted())
-      assertFalse(infraConfig.isInfra())
-      assertFalse(infraConfig.isRunningOnJenkinsInfra())
-      assertEquals('jenkins4eval', infraConfig.getDockerRegistry())
-    }
-
+    assertFalse(infraConfig.isTrusted())
+    assertFalse(infraConfig.isInfra())
+    assertFalse(infraConfig.isRunningOnJenkinsInfra())
+    assertEquals('jenkins4eval', infraConfig.getDockerRegistry())
+  }
 }
