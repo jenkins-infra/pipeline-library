0ea6042928ae319b006bee4b8e3dbbd32d0e3d5e
Hervé Le Meur <hlemeur@cloudbees.com>
2022-04-28 18:46:07 +0200

    wip

--- vars/buildDockerAndPublishImage.groovy
+++ vars/buildDockerAndPublishImage.groovy
@@ -56,7 +56,7 @@ def call(String imageName, Map userConfig=[:]) {
           if (operatingSystem == 'windows') {
             powershell 'Get-Variable'
             powershell 'dir env:'
-            powershell 'echo "$env:DOCKER_REGISTRY_PSW"'
+            powershell 'echo "$env:DOCKER_REGISTRY_USR"'
             powershell 'echo "SUITE"'
             // Logging in on the Dockerhub helps to avoid request limit from DockerHub
             powershell 'echo "$env:DOCKER_REGISTRY_PSW" | docker login -u "$env:DOCKER_REGISTRY_USR" --password-stdin'
