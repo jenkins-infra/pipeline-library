5c6b9b1738abd24f89d8397a5bd617e087492685
Hervé Le Meur <hlemeur@cloudbees.com>
2022-04-29 16:19:58 +0200

    wip

--- vars/buildDockerAndPublishImage.groovy
+++ vars/buildDockerAndPublishImage.groovy
@@ -183,16 +183,27 @@ def call(String imageName, Map userConfig=[:]) {
           // '''
           powershell '''
           	$dockerfile = ($env:WORKSPACE + "\\" + $env:IMAGE_DOCKERFILE.replace('/', '\\'))
-            echo "== Building $env:IMAGE_NAME from $env:IMAGE_DOCKERFILE..."
-            dir env:
-            echo "IMAGE_NAME: $env:IMAGE_NAME"
-            echo "IMAGE_PLATFORM: $env:IMAGE_PLATFORM"
-            echo "dockerfile: $dockerfile"
             $folder = (Split-Path -Path $dockerfile)
             $archive = "$folder\\image.tar"
-            echo "folder: $folder"
-            echo "archive: $archive"
-            docker build --tag $env:IMAGE_NAME --file $dockerfile $folder
+            echo "== Building $env:IMAGE_NAME from $env:IMAGE_DOCKERFILE..."
+
+            docker build \
+              --tag $env:IMAGE_NAME \
+              --build-arg "GIT_COMMIT_REV=$env:GIT_COMMIT_REV" \
+              --build-arg "GIT_SCM_URL=$env:GIT_SCM_URL" \
+              --build-arg "BUILD_DATE=$env:BUILD_DATE" \
+              --label "org.opencontainers.image.source=$env:GIT_SCM_URL" \
+              --label "org.label-schema.vcs-url=$env:GIT_SCM_URL" \
+              --label "org.opencontainers.image.url=$env:SCM_URI" \
+              --label "org.label-schema.url=$env:SCM_URI" \
+              --label "org.opencontainers.image.revision=$env:GIT_COMMIT_REV" \
+              --label "org.label-schema.vcs-ref=$env:GIT_COMMIT_REV" \
+              --label "org.opencontainers.image.created=$env:BUILD_DATE" \
+              --label "org.label-schema.build-date=$env:BUILD_DATE" \
+              # --platform $env:IMAGE_PLATFORM \
+              --file $dockerfile \
+              $folder
+
             echo "== Build Succeeded, image $env:IMAGE_NAME exported to $archive"
             dir
             echo "next..."
