5a33149171515a3080a08fa06deefbd59ec1c881
Hervé Le Meur <hlemeur@cloudbees.com>
2022-05-18 19:23:34 +0200

    powershell multiline

--- vars/buildDockerAndPublishImage.groovy
+++ vars/buildDockerAndPublishImage.groovy
@@ -54,7 +54,11 @@ def call(String imageName, Map userConfig=[:]) {
 
           // Logging in on the Dockerhub helps to avoid request limit from DockerHub
           if (operatingSystem == 'Windows') {
-            powershell 'Set-PSDebug -Trace 1; echo "$env:DOCKER_REGISTRY_PSW" | docker login -u "$env:DOCKER_REGISTRY_USR" --password-stdin'
+            powershell '''
+            Set-PSDebug -Trace 1
+            echo "$env:DOCKER_REGISTRY_PSW" | docker login -u "$env:DOCKER_REGISTRY_USR" \
+            --password-stdin
+            '''
             // powershell 'docker login -u "$env:DOCKER_REGISTRY_USR" -p "$env:DOCKER_REGISTRY_PSW"'// --password-stdin didn't worked on Windows
             // powershell "Set-PSDebug -Trace 1; ${env:CONTAINER_BIN} login -u \"${env:DOCKER_REGISTRY_USR}\" -p \"${env:DOCKER_REGISTRY_PSW}\""// --password-stdin didn't worked on Windows
             // Check login, should display the docker username
