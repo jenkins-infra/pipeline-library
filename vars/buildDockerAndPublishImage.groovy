51070b75c3fdba3a2dcb723d63088b7f4cdf15cb
Hervé Le Meur <hlemeur@cloudbees.com>
2022-05-19 14:49:19 +0200

    fix: only warn for linux/windows contradictions

--- vars/buildDockerAndPublishImage.groovy
+++ vars/buildDockerAndPublishImage.groovy
@@ -27,18 +27,14 @@ def call(String imageName, Map userConfig=[:]) {
   DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
   final String buildDate = dateFormat.format(now)
 
-  // Fix potential Linux/Windows contradictions between platform & agentLabels, and set the Windows config suffix for CST files
+  // Warn about potential Linux/Windows contradictions between platform & agentLabels, and set the Windows config suffix for CST files
   def cstConfigSuffix = ''
   if (finalConfig.agentLabels.contains('windows') || finalConfig.platform.contains('windows')) {
     if (finalConfig.agentLabels.contains('windows') && !finalConfig.platform.contains('windows')) {
       echo "WARNING: A 'windows' agent is requested, but the 'platform' is set to '${finalConfig.platform}'."
-      echo "Setting 'platform' to 'windows/amd64'"
-      finalConfig.platform = 'windows/amd64'
     }
     if (!finalConfig.agentLabels.contains('windows') && finalConfig.platform.contains('windows')) {
       echo "WARNING: The 'platform' is set to '${finalConfig.platform}', but there isn't any 'windows' agent requested."
-      echo "Adding 'windows' to the agent labels"
-      finalConfig.agentLabels += ' || windows'
     }
     cstConfigSuffix = '-windows'
   }
@@ -113,7 +109,7 @@ def call(String imageName, Map userConfig=[:]) {
         if (semVerEnabled) {
           stage("Semantic Release of ${imageName}") {
             echo "Configuring credential.helper"
-            if (isUnix) {
+            if (infra.isUnix) {
               sh 'git config --local credential.helper "!f() { echo username=\\$GIT_USERNAME; echo password=\\$GIT_PASSWORD; }; f"'
             } else {
               powershell 'git config --local credential.helper "!f() { echo username=%GIT_USERNAME%; echo password=%GIT_PASSWORD% }; f"'
