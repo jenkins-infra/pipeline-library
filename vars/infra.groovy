291479c7b8741149da98cd2a17a4363d4562f3ed
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-08-23 15:06:48 +0200

    debug: remove --no-transfert-progress

--- vars/infra.groovy
+++ vars/infra.groovy
@@ -112,7 +112,7 @@ boolean retrieveMavenSettingsFile(String settingsXml) {
  * @see #retrieveMavenSettingsFile(String)
  */
 Object runMaven(List<String> options, String jdk = '8', List<String> extraEnv = null, String settingsFile = null, Boolean addToolEnv = true) {
-  List<String> mvnOptions = ['--batch-mode', '--show-version', '--errors', '--no-transfer-progress']
+  List<String> mvnOptions = ['--batch-mode', '--show-version', '--errors']
   if (settingsFile) {
     mvnOptions += "-s $settingsFile"
   }
