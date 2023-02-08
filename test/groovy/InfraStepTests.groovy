a5d6478bcebb4ce91cb98d6586110092d2d455c6
Hervé Le Meur <hlemeur@cloudbees.com>
2023-02-08 17:55:25 +0100

    uncomment existing tests

--- test/groovy/InfraStepTests.groovy
+++ test/groovy/InfraStepTests.groovy
@@ -28,159 +28,159 @@ class InfraStepTests extends BaseTest {
     helper.addBatMock(prLabelsContainSkipACPScriptBat, '', 1)
   }
 
-  // @Test
-  // void testIsRunningOnJenkinsInfra() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   env.JENKINS_URL = 'https://ci.jenkins.io/'
-  //   assertTrue(script.isRunningOnJenkinsInfra())
-  // }
+  @Test
+  void testIsRunningOnJenkinsInfra() throws Exception {
+    def script = loadScript(scriptName)
+    env.JENKINS_URL = 'https://ci.jenkins.io/'
+    assertTrue(script.isRunningOnJenkinsInfra())
+  }
 
-  // @Test
-  // void testIsTrusted() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   env.JENKINS_URL = 'https://trusted.ci.jenkins.io:1443/'
-  //   binding.setVariable('env', env)
-  //   assertTrue(script.isTrusted())
-  // }
+  @Test
+  void testIsTrusted() throws Exception {
+    def script = loadScript(scriptName)
+    env.JENKINS_URL = 'https://trusted.ci.jenkins.io:1443/'
+    binding.setVariable('env', env)
+    assertTrue(script.isTrusted())
+  }
 
-  // @Test
-  // void testWithDockerCredentials() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   env.JENKINS_URL = 'https://ci.jenkins.io/'
-  //   def isOK = false
-  //   script.withDockerCredentials() {
-  //     isOK = true
-  //   }
-  //   printCallStack()
-  //   assertTrue(isOK)
-  //   assertJobStatusSuccess()
-  // }
+  @Test
+  void testWithDockerCredentials() throws Exception {
+    def script = loadScript(scriptName)
+    env.JENKINS_URL = 'https://ci.jenkins.io/'
+    def isOK = false
+    script.withDockerCredentials() {
+      isOK = true
+    }
+    printCallStack()
+    assertTrue(isOK)
+    assertJobStatusSuccess()
+  }
 
-  // @Test
-  // void testWithDockerCredentialsOutsideInfra() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   env.JENKINS_URL = 'https://foo/'
-  //   def isOK = false
-  //   script.withDockerCredentials() {
-  //     isOK = true
-  //   }
-  //   printCallStack()
-  //   assertFalse(isOK)
-  //   assertTrue(assertMethodCallContainsPattern('echo', 'Cannot use Docker credentials outside of jenkins infra environment'))
-  //   assertJobStatusSuccess()
-  // }
+  @Test
+  void testWithDockerCredentialsOutsideInfra() throws Exception {
+    def script = loadScript(scriptName)
+    env.JENKINS_URL = 'https://foo/'
+    def isOK = false
+    script.withDockerCredentials() {
+      isOK = true
+    }
+    printCallStack()
+    assertFalse(isOK)
+    assertTrue(assertMethodCallContainsPattern('echo', 'Cannot use Docker credentials outside of jenkins infra environment'))
+    assertJobStatusSuccess()
+  }
 
-  // @Test
-  // void testWithDockerPushCredentials() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   env.JENKINS_URL = 'https://ci.jenkins.io/'
-  //   def isOK = false
-  //   script.withDockerPushCredentials() {
-  //     isOK = true
-  //   }
-  //   printCallStack()
-  //   assertTrue(isOK)
-  //   assertJobStatusSuccess()
-  // }
+  @Test
+  void testWithDockerPushCredentials() throws Exception {
+    def script = loadScript(scriptName)
+    env.JENKINS_URL = 'https://ci.jenkins.io/'
+    def isOK = false
+    script.withDockerPushCredentials() {
+      isOK = true
+    }
+    printCallStack()
+    assertTrue(isOK)
+    assertJobStatusSuccess()
+  }
 
-  // @Test
-  // void testWithDockerPushCredentialsOutsideInfra() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   env.JENKINS_URL = 'https://foo/'
-  //   def isOK = false
-  //   script.withDockerPushCredentials() {
-  //     isOK = true
-  //   }
-  //   printCallStack()
-  //   assertFalse(isOK)
-  //   assertTrue(assertMethodCallContainsPattern('echo', 'Cannot use Docker credentials outside of jenkins infra environments'))
-  //   assertJobStatusSuccess()
-  // }
+  @Test
+  void testWithDockerPushCredentialsOutsideInfra() throws Exception {
+    def script = loadScript(scriptName)
+    env.JENKINS_URL = 'https://foo/'
+    def isOK = false
+    script.withDockerPushCredentials() {
+      isOK = true
+    }
+    printCallStack()
+    assertFalse(isOK)
+    assertTrue(assertMethodCallContainsPattern('echo', 'Cannot use Docker credentials outside of jenkins infra environments'))
+    assertJobStatusSuccess()
+  }
 
-  // @Test
-  // void testWithDockerPullCredentials() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   env.JENKINS_URL = 'https://ci.jenkins.io/'
-  //   def isOK = false
-  //   script.withDockerPullCredentials() {
-  //     isOK = true
-  //   }
-  //   printCallStack()
-  //   assertTrue(isOK)
-  //   assertJobStatusSuccess()
-  //   assertTrue(assertMethodCallContainsPattern('sh', 'echo "${DOCKER_CONFIG_PSW}" | "${CONTAINER_BIN}" login --username "${DOCKER_CONFIG_USR}" --password-stdin'))
-  // }
+  @Test
+  void testWithDockerPullCredentials() throws Exception {
+    def script = loadScript(scriptName)
+    env.JENKINS_URL = 'https://ci.jenkins.io/'
+    def isOK = false
+    script.withDockerPullCredentials() {
+      isOK = true
+    }
+    printCallStack()
+    assertTrue(isOK)
+    assertJobStatusSuccess()
+    assertTrue(assertMethodCallContainsPattern('sh', 'echo "${DOCKER_CONFIG_PSW}" | "${CONTAINER_BIN}" login --username "${DOCKER_CONFIG_USR}" --password-stdin'))
+  }
 
-  // @Test
-  // void testWithDockerPullCredentialsWindows() throws Exception {
-  //   helper.registerAllowedMethod('isUnix', [], { false })
-  //   def script = loadScript(scriptName)
-  //   env.JENKINS_URL = 'https://ci.jenkins.io/'
-  //   def isOK = false
-  //   script.withDockerPullCredentials() {
-  //     isOK = true
-  //   }
-  //   printCallStack()
-  //   assertTrue(isOK)
-  //   assertJobStatusSuccess()
-  //   assertTrue(assertMethodCallContainsPattern('powershell', 'Invoke-Expression "${Env:CONTAINER_BIN} login --username ${Env:DOCKER_CONFIG_USR} --password ${Env:DOCKER_CONFIG_PSW}"'))
-  // }
+  @Test
+  void testWithDockerPullCredentialsWindows() throws Exception {
+    helper.registerAllowedMethod('isUnix', [], { false })
+    def script = loadScript(scriptName)
+    env.JENKINS_URL = 'https://ci.jenkins.io/'
+    def isOK = false
+    script.withDockerPullCredentials() {
+      isOK = true
+    }
+    printCallStack()
+    assertTrue(isOK)
+    assertJobStatusSuccess()
+    assertTrue(assertMethodCallContainsPattern('powershell', 'Invoke-Expression "${Env:CONTAINER_BIN} login --username ${Env:DOCKER_CONFIG_USR} --password ${Env:DOCKER_CONFIG_PSW}"'))
+  }
 
-  // @Test
-  // void testWithDockerPullCredentialsOutsideInfra() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   env.JENKINS_URL = 'https://foo/'
-  //   def isOK = false
-  //   script.withDockerPullCredentials() {
-  //     isOK = true
-  //   }
-  //   printCallStack()
-  //   assertFalse(isOK)
-  //   assertTrue(assertMethodCallContainsPattern('echo', 'Cannot use Docker credentials outside of jenkins infra environments'))
-  //   assertJobStatusSuccess()
-  // }
+  @Test
+  void testWithDockerPullCredentialsOutsideInfra() throws Exception {
+    def script = loadScript(scriptName)
+    env.JENKINS_URL = 'https://foo/'
+    def isOK = false
+    script.withDockerPullCredentials() {
+      isOK = true
+    }
+    printCallStack()
+    assertFalse(isOK)
+    assertTrue(assertMethodCallContainsPattern('echo', 'Cannot use Docker credentials outside of jenkins infra environments'))
+    assertJobStatusSuccess()
+  }
 
-  // @Test
-  // void testCheckoutWithEnvVariable() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   env.BRANCH_NAME = 'BRANCH'
-  //   script.checkoutSCM()
-  //   printCallStack()
-  //   assertJobStatusSuccess()
-  // }
+  @Test
+  void testCheckoutWithEnvVariable() throws Exception {
+    def script = loadScript(scriptName)
+    env.BRANCH_NAME = 'BRANCH'
+    script.checkoutSCM()
+    printCallStack()
+    assertJobStatusSuccess()
+  }
 
-  // @Test
-  // void testCheckoutWithArgument() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   script.checkoutSCM('foo.git')
-  //   printCallStack()
-  //   assertJobStatusSuccess()
-  // }
+  @Test
+  void testCheckoutWithArgument() throws Exception {
+    def script = loadScript(scriptName)
+    script.checkoutSCM('foo.git')
+    printCallStack()
+    assertJobStatusSuccess()
+  }
 
-  // @Test
-  // void testCheckoutWithoutArgument() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   try {
-  //     script.checkoutSCM()
-  //   } catch(e) {
-  //     //NOOP
-  //   }
-  //   printCallStack()
-  //   assertTrue(assertMethodCallContainsPattern('error', 'buildPlugin must be used as part of a Multibranch Pipeline *or* a `repo` argument must be provided'))
-  //   assertJobStatusFailure()
-  // }
+  @Test
+  void testCheckoutWithoutArgument() throws Exception {
+    def script = loadScript(scriptName)
+    try {
+      script.checkoutSCM()
+    } catch(e) {
+      //NOOP
+    }
+    printCallStack()
+    assertTrue(assertMethodCallContainsPattern('error', 'buildPlugin must be used as part of a Multibranch Pipeline *or* a `repo` argument must be provided'))
+    assertJobStatusFailure()
+  }
 
-  // @Test
-  // void testRetrieveMavenSettingsFileWithEnvVariable() throws Exception {
-  //   def script = loadScript(scriptName)
-  //   env.MAVEN_SETTINGS_FILE_ID = 'foo.id'
-  //   def result = script.retrieveMavenSettingsFile('foo.xml')
-  //   assertTrue(result)
-  //   printCallStack()
-  //   assertJobStatusSuccess()
-  //   assertTrue(assertMethodCallContainsPattern('sh', 'settings.xml foo.xml'))
-  //   assertTrue(assertMethodCallContainsPattern('configFile', 'foo.id'))
-  // }
+  @Test
+  void testRetrieveMavenSettingsFileWithEnvVariable() throws Exception {
+    def script = loadScript(scriptName)
+    env.MAVEN_SETTINGS_FILE_ID = 'foo.id'
+    def result = script.retrieveMavenSettingsFile('foo.xml')
+    assertTrue(result)
+    printCallStack()
+    assertJobStatusSuccess()
+    assertTrue(assertMethodCallContainsPattern('sh', 'settings.xml foo.xml'))
+    assertTrue(assertMethodCallContainsPattern('configFile', 'foo.id'))
+  }
 
   @Test
   void testWithArtifactCachingProxy() throws Exception {
