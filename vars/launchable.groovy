4c332679d6746c642b2752d08217f6fafda8d6c7
Basil Crow <me@basilcrow.com>
2023-03-30 14:14:19 -0700

    Make Launchable errors non-fatal (#634)

--- vars/launchable.groovy
+++ vars/launchable.groovy
@@ -1,10 +1,12 @@
 def install() {
   if (isUnix()) {
-    sh '''
-        python3 -m venv launchable
-        launchable/bin/pip --require-virtualenv --no-cache-dir install -U setuptools wheel
-        launchable/bin/pip --require-virtualenv --no-cache-dir install launchable
-       '''.stripIndent()
+    catchError(buildResult: 'SUCCESS', catchInterruptions: false, message: 'Failed to install Launchable; continuing build.') {
+      sh '''
+          python3 -m venv launchable
+          launchable/bin/pip --require-virtualenv --no-cache-dir install -U setuptools wheel
+          launchable/bin/pip --require-virtualenv --no-cache-dir install launchable
+         '''.stripIndent()
+    }
   } else {
     error 'Launchable installation is not yet implemented for Windows'
   }
@@ -13,7 +15,9 @@ def install() {
 def call(String args) {
   withCredentials([string(credentialsId: 'launchable-prototype', variable: 'LAUNCHABLE_TOKEN')]) {
     if (isUnix()) {
-      sh 'launchable/bin/launchable ' + args
+      catchError(buildResult: 'SUCCESS', catchInterruptions: false, message: 'Failed to run Launchable; continuing build.') {
+        sh 'launchable/bin/launchable ' + args
+      }
     } else {
       error 'The Launchable CLI is not yet implemented for Windows'
     }
