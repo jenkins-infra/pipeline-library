64896b258b03a0eb44525ab1af6f6fab9a850b19
Hervé Le Meur <hlemeur@cloudbees.com>
2023-03-31 14:05:43 +0200

    remove unused comment function from the PullRequest mock

--- test/groovy/mock/PullRequest.groovy
+++ test/groovy/mock/PullRequest.groovy
@@ -4,15 +4,10 @@ package mock
  * Mock PullRequest
  */
 class PullRequest implements Serializable {
-  String comment
   def labels
 
   public PullRequest(def labels = []) {
     this.labels = labels
-  }
-
-  public comment(String comment) {
-    this.comment = comment
   }
 
   public def labels() {
