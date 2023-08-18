5f56408176b6277fe2f946cbedd01c08f0275d5a
smerle33 <stephane.merle+jenkins@gmail.com>
2023-08-18 17:53:46 +0200

    change tag to remove beta

--- vars/buildDockerAndPublishImage.groovy
+++ vars/buildDockerAndPublishImage.groovy
@@ -22,7 +22,7 @@ def call(String imageShortName, Map userConfig=[:]) {
   final Map finalConfig = defaultConfig << userConfig
 
   // DEBUG ONLY FOR PUSH
-  env.TAG_NAME = "0.0.1-beta"
+  env.TAG_NAME = "0.0.1"
 
   // Retrieve Library's Static File Resources
   final String makefileContent = libraryResource 'io/jenkins/infra/docker/Makefile'
