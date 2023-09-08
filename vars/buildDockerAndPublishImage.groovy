a64a0f15cd84da2f3b5992ebe67a561dce6cbde7
smerle33 <stephane.merle+jenkins@gmail.com>
2023-09-08 09:17:37 +0200

    add windows debug

--- vars/buildDockerAndPublishImage.groovy
+++ vars/buildDockerAndPublishImage.groovy
@@ -187,6 +187,8 @@ def call(String imageShortName, Map userConfig=[:]) {
               if (isUnix()) {
                 sh 'make lint'
               } else {
+                powershell "$pwd"
+                powershell 'dir c:\maven\jdk11'
                 powershell 'make lint'
               }
             } finally {
