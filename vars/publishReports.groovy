20f23e87379fac20586f9fcc67a97d4b55363f5b
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2024-05-02 21:11:36 +0200

    feat(publishReports): add `failIfEmpty` parameter

--- vars/publishReports.groovy
+++ vars/publishReports.groovy
@@ -3,10 +3,14 @@
 /**
  * Wrapper for publishing reports to the reports host
  *
+ * @param timeout: timout in minutes for the blob storage upload (default: 60)
+ * @param failIfEmpty: set to true to fail if the file to upload isn't empty (default: false)
+ *
  * See https://issues.jenkins-ci.org/browse/INFRA-947 for more
  */
 def call(List<String> files, Map params = [:]) {
   def timeout = params.get('timeout') ?: '60'
+  def failIfEmpty = params.get('failIfEmpty') ?: false
 
   if (!infra.isTrusted() && !infra.isInfra()) {
     error 'Can only call publishReports from within the trusted.ci environment'
@@ -52,11 +56,20 @@ def call(List<String> files, Map params = [:]) {
           "DESTINATION_PATH=${dirname ?: '/'}",
           "PATTERN=${ basename ?: '*' }",
         ]) {
-          // Blob container can be removed once files are uploaded on the azure file storage
-          sh 'az storage blob upload --account-name=prodjenkinsreports --container=reports --timeout=${TIMEOUT} --file=${FILENAME} --name=${FILENAME} ${UPLOADFLAGS} --overwrite'
+          sh '''
+          # If the "failIsEmpty" flag is active and the file is empty, fail the pipeline
+          lineCount=$(wc -l "${filename}" | awk '{print $1}')
+          if [[ "${failIsEmpty}" != false && "${lineCount}" -eq 0 ]]; then
+            echo "ERROR: ${filename} is empty."
+            exit 1
+          else
+            # Blob container can be removed once files are uploaded on the azure file storage
+            az storage blob upload --account-name=prodjenkinsreports --container=reports --timeout="${TIMEOUT}" --file="${FILENAME}" --name="${FILENAME}" "${UPLOADFLAGS}" --overwrite
 
-          // `az storage file upload` doesn't support file uploaded in a remote directory that doesn't exist but upload-batch yes. Unfortunately the cli syntax is a bit different and requires filename and directory name to be set differently.
-          sh 'az storage file upload-batch --account-name prodjenkinsreports --destination reports --source ${SOURCE_DIRNAME} --destination-path ${DESTINATION_PATH} --pattern ${PATTERN} ${UPLOADFLAGS}'
+            # `az storage file upload` doesn't support file uploaded in a remote directory that doesn't exist but upload-batch yes. Unfortunately the cli syntax is a bit different and requires filename and directory name to be set differently.
+            az storage file upload-batch --account-name prodjenkinsreports --destination reports --source "${SOURCE_DIRNAME}" --destination-path "${DESTINATION_PATH}" --pattern "${PATTERN}" "${UPLOADFLAGS}"
+          fi
+          '''
         }
       }
     }
