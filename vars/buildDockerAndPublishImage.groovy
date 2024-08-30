jenkins-infra/pipeline-library

commit cab6197808bf7b945c3dc4d952031c7ae6f92574

feat(BuildDockerAndPublishImage): publish checks only on other branch than `main` to avoid GH rate limit (#879)
* feat(BuildDockerAndPublishImage): publish checks only on other branch than Main to avoid GH rate limit

* chore

* Update vars/buildDockerAndPublishImage.groovy

Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

---------

Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

vars/buildDockerAndPublishImage.groovy
@@ -196,6 +196,7 @@ def call(String imageShortName, Map userConfig=[:]) {
               }
             } finally {
               recordIssues(
+                  skipPublishingChecks: env.BRANCH_IS_PRIMARY,
                   enabledForFailure: true,
                   aggregatingResults: false,
                   tool: hadoLint(id: hadolintReportId, pattern: hadoLintReportFile)

test/groovy/BuildDockerAndPublishImageStepTests.groovy
@@ -119,7 +119,7 @@ class BuildDockerAndPublishImageStepTests extends BaseTest {
     final String reportId = "${imageName}-hadolint-${mockedTimestamp}".replaceAll('/','-').replaceAll(':', '-')
     return assertMethodCallContainsPattern(
         'recordIssues',
-        "{enabledForFailure=true, aggregatingResults=false, tool={id=${reportId}, pattern=${reportId}.json}}",
+        "{skipPublishingChecks=${env.BRANCH_IS_PRIMARY}, enabledForFailure=true, aggregatingResults=false, tool={id=${reportId}, pattern=${reportId}.json}}",
         )
   }
 

commit 303b665a2c86a3451e0f2ad23aa8382a014454ac

fix(buildDockerAndPublishImage) avoid wrong type inference for `recordIssues(skipPublishingChecks=)` argument
This change fixes the error below caused by #879 

```
Found unhandled java.lang.IllegalArgumentException exception:

Could not instantiate {skipPublishingChecks=null, enabledForFailure=true, aggregatingResults=false, tool=@hadoLint(id=jenkinsciinfra-openvpn-hadolint-1725004944771,pattern=jenkinsciinfra-openvpn-hadolint-1725004944771.json)} for io.jenkins.plugins.analysis.core.steps.RecordIssuesStep: java.lang.IllegalArgumentException
```

vars/buildDockerAndPublishImage.groovy
@@ -195,8 +195,12 @@ def call(String imageShortName, Map userConfig=[:]) {
                 powershell 'make lint'
               }
             } finally {
+              boolean skipChecks = false
+              if (env.BRANCH_IS_PRIMARY) {
+                skipChecks = true
+              }
               recordIssues(
-                  skipPublishingChecks: env.BRANCH_IS_PRIMARY,
+                  skipPublishingChecks: skipChecks,
                   enabledForFailure: true,
                   aggregatingResults: false,
                   tool: hadoLint(id: hadolintReportId, pattern: hadoLintReportFile)

test/groovy/BuildDockerAndPublishImageStepTests.groovy
@@ -117,9 +117,13 @@ class BuildDockerAndPublishImageStepTests extends BaseTest {
   // Return if the usual static checks had been recorded with the usual pattern
   Boolean assertRecordIssues(String imageName = fullTestImageName) {
     final String reportId = "${imageName}-hadolint-${mockedTimestamp}".replaceAll('/','-').replaceAll(':', '-')
+    boolean skipChecks = false
+    if (env.BRANCH_IS_PRIMARY) {
+      skipChecks = true
+    }
     return assertMethodCallContainsPattern(
         'recordIssues',
-        "{skipPublishingChecks=${env.BRANCH_IS_PRIMARY}, enabledForFailure=true, aggregatingResults=false, tool={id=${reportId}, pattern=${reportId}.json}}",
+        "{skipPublishingChecks=${skipChecks}, enabledForFailure=true, aggregatingResults=false, tool={id=${reportId}, pattern=${reportId}.json}}",
         )
   }
 