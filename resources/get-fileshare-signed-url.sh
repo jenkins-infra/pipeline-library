c6313507a42e33762364ec920b4fa6d56b91e79b
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2025-12-18 14:53:56 +0100

    rework script with early return when credential-less, and proper script desc and doc

--- resources/get-fileshare-signed-url.sh
+++ resources/get-fileshare-signed-url.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-# Purpose: Shell script to get a file share URL signed with a short-lived SAS token
+# Purpose: Shell script to get a file share URL signed or not with a short-lived SAS token
 # --
-# Description: This script uses either a service principal or either a storage account access key to generate a SAS token
-# and returns the file share URL composed of the storage resource URI and the SAS token.
+# Description: This script uses a service principal or a storage account access key to generate a SAS token
+# and returns a signed file share URL composed of the storage resource URI and the SAS token,
+# or login with azcopy and the service principal from a VM agent's user assigned identity (credential-less) and returns an unsigned file share URL if no service principal secret is passed.
 # Ref: https://learn.microsoft.com/en-us/azure/storage/common/storage-sas-overview
-# This service principal can come from a credentials, or from a VM agent's user assigned identity
 # --
 # Usage:
-# - Return a file share signed URL: ./get-fileshare-signed-url.sh
+# - Return a file share URL, signed if a service principal credential secret is passed: ./get-fileshare-signed-url.sh
 # - Interact with a file share and azcopy: azcopy list "$(./get-fileshare-signed-url.sh)"
 # --
 # Required parameters defined as environment variables:
 # - STORAGE_FILESHARE: the file share name
 # - STORAGE_NAME: the storage account name where the file share is located
-# - STORAGE_DURATION_IN_MINUTE: lifetime of the short-lived SAS token, in minute
-# - STORAGE_PERMISSIONS: the permission(s) granted on the file share, any of "dlrw" (note: the order matters)
+# - STORAGE_DURATION_IN_MINUTE: lifetime of the short-lived SAS token, in minute. Note: not taken in account in credential-less case
+# - STORAGE_PERMISSIONS: the permission(s) granted on the file share, any of "dlrw" (note: the order matters). Note: not taken in account in credential-less case
 #
-# Depending on wether you want to use a service principal or an access key to generate the SAS token, you'll also need either:
+# When not in credential-less, depending on wether you want to use a service principal or an access key to generate the SAS token, you'll also need either:
 # - AZURE_STORAGE_KEY: the storage account access key
 # or
 # - JENKINS_INFRA_FILESHARE_CLIENT_ID: the service principal app registration client id
@@ -37,18 +37,22 @@ export AZURE_CONFIG_DIR
 
 secret="${JENKINS_INFRA_FILESHARE_CLIENT_SECRET:-}"
 
-accountKeyArg=()
-shouldLogout="false"
-generate_sas_token="false"
-# If a storage account key env var exists, use it instead of a service principal to generate a file share SAS token
-if  [[ -n "${AZURE_STORAGE_KEY:-}" ]]; then
-    accountKeyArg=("--account-key" "${AZURE_STORAGE_KEY}")
-    shouldLogout="false"
-    generate_sas_token="true"
+fileshare_url="https://${STORAGE_NAME}.file.core.windows.net/${STORAGE_FILESHARE}"
+
+# Credential-less using user assigned identity, no need for any SAS token in the returned URL
+if [[ -z "${secret}" ]]; then
+    # Login without the JSON output from azcopy
+    azcopy login --identity > /dev/null
+    echo "${fileshare_url}"
+    exit 0
 fi
 
-# If a fileshare client secret is defined, use it to login
-if [[ -n "${secret}" ]]; then
+accountKeyArg=()
+shouldLogout="false"
+# If a storage account key env var exists, use it instead of a service principal to generate the file share SAS token
+if  [[ -n "${AZURE_STORAGE_KEY:-}" ]]; then
+    accountKeyArg=("--account-key" "${AZURE_STORAGE_KEY}")
+else
     # Env vars needed to use a service principal
     : "${JENKINS_INFRA_FILESHARE_CLIENT_ID?}" "${JENKINS_INFRA_FILESHARE_TENANT_ID?}"
 
@@ -59,31 +63,24 @@ if [[ -n "${secret}" ]]; then
     --tenant "${JENKINS_INFRA_FILESHARE_TENANT_ID}" > /dev/null
 
     shouldLogout="true"
-    generate_sas_token="true"
 fi
 
-# If using an Azure storage key or an Azure credentials, we need to generate a Service SAS token to get a signed File Share URL
-query_string=""
-if [[ "${generate_sas_token}" == "true" ]]; then
-    # date(1) isn't GNU compliant on MacOS, using gdate(1) in that case
-    [[ "$(uname  || true)" == "Darwin" ]] && dateCmd="gdate" || dateCmd="date"
-    expiry="$("${dateCmd}" --utc --date "+ ${STORAGE_DURATION_IN_MINUTE} minutes" +"%Y-%m-%dT%H:%MZ")"
+# date(1) isn't GNU compliant on MacOS, using gdate(1) in that case
+[[ "$(uname  || true)" == "Darwin" ]] && dateCmd="gdate" || dateCmd="date"
+expiry="$("${dateCmd}" --utc --date "+ ${STORAGE_DURATION_IN_MINUTE} minutes" +"%Y-%m-%dT%H:%MZ")"
 
-    # Generate a SAS token, remove double quotes around it and replace potential '/' by '%2F'
-    query_string="/?$(az storage share generate-sas "${accountKeyArg[@]}" \
-    --name "${STORAGE_FILESHARE}" \
-    --account-name "${STORAGE_NAME}" \
-    --https-only \
-    --permissions "${STORAGE_PERMISSIONS}" \
-    --expiry "${expiry}" \
-    --only-show-errors \
-    | sed 's/\"//g' \
-    | sed 's|/|%2F|g')"
-else
-    # If using user assigned identity, we just have to use it to login with azcopy
-    azcopy login --identity > /dev/null
-fi
+# Generate a SAS token, remove double quotes around it and replace potential '/' by '%2F'
+token="/?$(az storage share generate-sas "${accountKeyArg[@]}" \
+--name "${STORAGE_FILESHARE}" \
+--account-name "${STORAGE_NAME}" \
+--https-only \
+--permissions "${STORAGE_PERMISSIONS}" \
+--expiry "${expiry}" \
+--only-show-errors \
+| sed 's/\"//g' \
+| sed 's|/|%2F|g')"
 
 [[ "${shouldLogout}" == "true" ]] && az logout
 
-echo "https://${STORAGE_NAME}.file.core.windows.net/${STORAGE_FILESHARE}${query_string}"
+# Return signed URL
+echo "${fileshare_url}/?${token}"
