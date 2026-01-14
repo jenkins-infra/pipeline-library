ddb5a57027ac955d9b63e7477096e87fc1dbc306
Damien Duportal <damien.duportal@gmail.com>
2026-01-14 14:12:33 +0100

    Update publishReports.groovy

--- vars/publishReports.groovy
+++ vars/publishReports.groovy
@@ -64,14 +64,13 @@ def call(List<String> files, Map params = [:]) {
           "IS_CREDENTIAL_LESS=${params.useWorkloadIdentity.toString()}",
         ]) {
           sh '''
-          # Don't output sensitive information
-          set +x
-
           if [[ "${IS_CREDENTIAL_LESS}" == "true" ]]
           then
             # No query string (but a trailing slash in 'FILESHARE_SIGNED_URL')
             fileShareUrl="${FILESHARE_SIGNED_URL}${DESTINATION_PATH}"
           else
+            # Don't output sensitive information such as the SAS token in the querystring
+            set +x
             fileShareUrl="$(echo "${FILESHARE_SIGNED_URL}" | sed "s#/?#/${DESTINATION_PATH}?#")"
           fi
 
