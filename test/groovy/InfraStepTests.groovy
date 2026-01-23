49ca3ab2766e2f4962ca6de12fa1c242c79c71bc
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2026-01-23 14:36:04 +0100

    chore(infra.getBuildAgentLabel): extract `containerAgentLabel` and `vmAgentLabel` as private functions (#981)

--- test/groovy/InfraStepTests.groovy
+++ test/groovy/InfraStepTests.groovy
@@ -528,6 +528,7 @@ class InfraStepTests extends BaseTest {
       [platform: 'windows', jdk: '8',  container: false, expected: 'docker-windows',   warning: null],
       // unknown platform
       [platform: 'openbsd', jdk: '11', container: false, expected: 'openbsd',          warning: 'vm'],
+      [platform: 'openbsd', jdk: '11', container: true,  expected: 'openbsd',          warning: 'container'],
     ]
 
     cases.each { c ->
