ba892abf3082965c4ce8a35a6958dd5ec7d9e928
Jesse Glick <jglick@cloudbees.com>
2026-02-13 18:23:34 -0500

    Wrong step name

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -316,7 +316,7 @@ def call(Map params = [:]) {
     infra.maybePublishIncrementals()
   }
   if (consumingIncrementals) {
-    warning 'This build consumed Incremental dependencies. Remove the `consume-incrementals` file before reading for review.'
+    unstable 'This build consumed Incremental dependencies. Remove the `consume-incrementals` file before reading for review.'
   }
 }
 
