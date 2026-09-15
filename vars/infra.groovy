14794596dccc43bd106407a6d58883ab757da313
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-09-15 17:11:31 +0200

    mvn spotless:apply

--- vars/infra.groovy
+++ vars/infra.groovy
@@ -817,10 +817,7 @@ private void deployToNetlify(Map params = [:]) {
     error 'A netlify site name is required'
   }
   withCredentials([string(credentialsId: 'netlify-auth-token', variable: 'NETLIFY_AUTH_TOKEN')]) {
-    withEnv([
-      "NETLIFY_NAME=${config.netlifyName}",
-      "PUBLIC_FOLDER=${params.deployFolder}",
-    ]) {
+    withEnv(["NETLIFY_NAME=${config.netlifyName}", "PUBLIC_FOLDER=${params.deployFolder}",]) {
       String netlifyCommand = 'netlify-deploy --draft=true --siteName "${NETLIFY_NAME}" --title "Preview deploy for ${CHANGE_ID}" --alias "deploy-preview-${CHANGE_ID}" -d "${PUBLIC_FOLDER}"'
       String recordRef = pullRequest.head
       String recordUrl = "https://deploy-preview-${env.CHANGE_ID}--${config.netlifyName}.netlify.app"
